23f04aec30ee8aa81a7d610b4735e7da71241375
Ambroos Vaes <ambroosv@gmail.com>
2013-11-18 21:53:07 +0100

    v1.1 Added hiding of app and Dutch translation

--- src/be/ambroosvaes/xperiascrobbler/Z1Compat/MainActivity.java
+++ src/be/ambroosvaes/xperiascrobbler/Z1Compat/MainActivity.java
@@ -2,8 +2,12 @@ package be.ambroosvaes.xperiascrobbler.Z1Compat;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
@@ -15,13 +19,34 @@ public class MainActivity extends Activity {
 		if (!appInstalledOrNot("fm.last.android")) {
 			Intent intent = new Intent(Intent.ACTION_VIEW);
 			intent.setData(Uri.parse("market://details?id=fm.last.android"));
-			Toast toast = Toast.makeText(getApplicationContext(), R.string.install_last_fm_toast, Toast.LENGTH_LONG);
+			Toast toast = Toast.makeText(getApplicationContext(),
+					R.string.install_last_fm_toast, Toast.LENGTH_LONG);
 			toast.show();
 			startActivity(intent);
 			finish();
 		}
 		setContentView(R.layout.activity_main);
-		
+
+	}
+
+	public void hideApp(View v) {
+		AlertDialog.Builder builder = new Builder(this);
+		builder.setMessage(R.string.confirm_hide);
+		builder.setTitle(R.string.confirm_hide_title);
+		builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	   PackageManager p = getPackageManager();
+		       			p.setComponentEnabledSetting(getComponentName(),
+		       				PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+		           }
+		       });
+		builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		               // User cancelled the dialog
+		           }
+		       });
+		// Create the AlertDialog
+		builder.show();
 	}
 
 	private boolean appInstalledOrNot(String uri) {
@@ -36,6 +61,7 @@ public class MainActivity extends Activity {
 		return app_installed;
 	}
 	
+
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
